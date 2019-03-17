53c1f4f6680b7e2e44ad6dc947578abba3d48b62
Jonathan Hsu <jon@high5.ai>
2019-03-17 14:37:43 +0800

    finish echo bot

--- app.js
+++ app.js
@@ -1,27 +1,16 @@
-const axios = require('axios')
-const dotenv = require('dotenv')
 const express = require('express')
-
-dotenv.config()
 const app = express()
-const TOKEN = process.env.TOKEN
+
+const reply = require('./features/reply')
 
 app.use(express.json())
 
-app.post('/', async function (req, res) {
-    let headers = { 'Authorization': 'Bearer ' + TOKEN }
-    let event = req.body.events[0]
-    let body = {
-        replyToken: event.replyToken,
-        messages: [{
-            type: 'text',
-            text: event.message.text
-        }]
-    }
-    await axios.post('https://api.line.me/v2/bot/message/reply', headers, body)
-    res.sendStatus(200)
+app.get('/', (req, res) => {
+    res.send('Stand by.')
 })
 
-app.listen(3000, function () {
+app.post('/', reply.echo)
+
+app.listen(3000, () => {
     console.log('TARS listening on port 3000!')
 })
